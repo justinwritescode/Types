b9aa81a8f0248e650404e1fedc45053ecbfd7d9f
David <justin@justinwritescode.com>
2022-12-24 01:25:00 -0500

    Changes to tuples

--- src/Core/Tuples/UriStringTuple.cs
+++ src/Core/Tuples/UriStringTuple.cs
@@ -2,23 +2,27 @@ namespace JustinWritesCode.Tuples;
 
 public abstract class UriStringTuple : Tuple<uri?, string?>
 {
-    public UriStringTuple(Uri? uri = default, string? value = default) : base(uri, value) { }
-    public Uri? Uri => Item1;
+    public UriStringTuple(uri? uri = default, string? value = default) : base(uri, value) { }
+    public uri? Uri => Item1;
     public UriStringTuple(string? uri = default, string? value = default) : this(uri?.ToUri(), value) { }
     public UriStringTuple(UriStringTuple? tuple = default) : this(tuple?.Item1, tuple?.Item2) { }
-    public UriStringTuple((Uri?, string?)? tuple = default) : this(tuple?.Item1, tuple?.Item2) { }
+#if NETSTANDARD2_0_OR_GREATER
+    public UriStringTuple((uri?, string?)? tuple = default) : this(tuple?.Item1, tuple?.Item2) { }
     public UriStringTuple((string?, string?)? tuple = default) : this(tuple?.Item1, tuple?.Item2) { }
-    public UriStringTuple() : this(null as uri, null as string) { }
+#endif
+    public UriStringTuple() : this(null as uri?, null as string) { }
 }
 
 public abstract class UriDescriptionTuple : UriStringTuple
 {
     public virtual string? Description => Item2;
-    protected UriDescriptionTuple(Uri uri, string? description = default) : base(uri, description) { }
+    protected UriDescriptionTuple(uri? uri, string? description = default) : base(uri, description) { }
     protected UriDescriptionTuple(string uri, string? description = default) : this(uri.ToUri(), description) { }
+#if NETSTANDARD2_0_OR_GREATER
     protected UriDescriptionTuple((uri?, string?)? tuple = default) : base(tuple?.Item1, tuple?.Item2) { }
     protected UriDescriptionTuple((string?, string?)? tuple = default) : base(tuple?.Item1, tuple?.Item2) { }
 
+#endif
     public static implicit operator string?(UriDescriptionTuple tuple) => tuple.Uri.ToString();
-    public static implicit operator Uri?(UriDescriptionTuple tuple) => tuple.Uri;
+    public static implicit operator uri?(UriDescriptionTuple tuple) => tuple.Uri;
 }

24c6b7dc8242059da1e833c50a29181b5e13a219
David <justin@justinwritescode.com>
2022-12-24 01:26:23 -0500

    Constants

--- src/Constants/Uris.cs
+++ src/Constants/Uris.cs
@@ -6,12 +6,12 @@ public static class Constants
     {
         public static class Strings
         {
-            /// <summary>The base URI for justinwritesode, <inheritdoc cref="JustinWritesCodeUriBaseString" path="/value/node()" /></summary>
+            /// <summary>The base URI for justinwritesode <inheritdoc cref="JustinWritesCodeBaseUriString" path="/value" /></summary>
             /// <value><c>https://justinwritescode.com/</c></value>
             public const string JustinWritesCodeBaseUriString = "https://justinwritescode.com/";
         }
 
         /// <inheritdoc cref="Strings.JustinWritesCodeBaseUriString" />
-        public static readonly uri JustinWritesCodeBaseUri = new(Strings.JustinWritesCodeBaseUriString);
+        public static readonly uri JustinWritesCodeBaseUri = uri.From(Strings.JustinWritesCodeBaseUriString);
     }
 }

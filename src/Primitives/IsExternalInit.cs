4a863b410a2e72b9811ae8ee79ed2c30f4b29b28
David <justin@justinwritescode.com>
2023-01-04 21:53:29 -0500

    Update IsExternalInit.cs

--- src/Primitives/IsExternalInit.cs
+++ src/Primitives/IsExternalInit.cs
@@ -1,15 +1,3 @@
-/* 
- * IsExternalInit copy.cs
- * 
- *   Created: 2022-12-28-07:13:20
- *   Modified: 2022-12-28-07:13:20
- * 
- *   Author: Justin Chase <justin@justinwritescode.com>
- *   
- *   Copyright © 2022 Justin Chase, All Rights Reserved
- *      License: MIT (https://opensource.org/licenses/MIT)
- */ 
-
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
